53d5c186f4f8cce3652677de70ff225bb84d83eb
leehayeong <lhaayyy@naver.com>
2022-06-02 18:08:07 +0900

    os 8 이하 처리를 위해 ShortcutInfoCompat, ShortcutManagerCompat 사용

--- app/src/main/java/com/example/shortcutpractice/MainActivity.kt
+++ app/src/main/java/com/example/shortcutpractice/MainActivity.kt
@@ -8,6 +8,7 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.pm.ShortcutInfoCompat
 import androidx.core.content.pm.ShortcutManagerCompat
 import androidx.databinding.DataBindingUtil
 import com.example.shortcutpractice.databinding.ActivityMainBinding
@@ -24,30 +25,49 @@ class MainActivity : AppCompatActivity() {
 
     private fun setPinnedShortcut() {
         binding.pinnedShortcutBtn.setOnClickListener {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                val shortcutManager = getSystemService(ShortcutManager::class.java)
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//                val shortcutManager = getSystemService(ShortcutManager::class.java)
+//
+//                if (shortcutManager!!.isRequestPinShortcutSupported) {
+//                    val pinShortcutInfo =
+//                        ShortcutInfo.Builder(this, "my-shortcut")
+//                            .setShortLabel("pinned shortcut")
+//                            .setIntent(Intent(Intent.ACTION_VIEW, Uri.parse("pinned://deeplink")))
+//                            .build()
+//
+//                    val pinnedShortcutCallbackIntent =
+//                        shortcutManager.createShortcutResultIntent(pinShortcutInfo)
+//
+//                    val successCallback = PendingIntent.getBroadcast(
+//                        this,
+//                        0,
+//                        pinnedShortcutCallbackIntent,
+//                        0)
+//
+//                    shortcutManager.requestPinShortcut(pinShortcutInfo, successCallback.intentSender)
+//                }
+//            } else {
+//                // asis code
+//                val shortcutIntent = Intent()
+//                shortcutIntent.action = Intent.ACTION_VIEW
+//                shortcutIntent.addCategory(Intent.CATEGORY_DEFAULT)
+//                shortcutIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+//
+//                val shortcut = Intent("com.android.launcher.action.INSTALL_SHORTCUT")
+//                shortcut.putExtra("duplicate", false)
+//                shortcut.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent)
+//                shortcut.putExtra(Intent.EXTRA_SHORTCUT_NAME, "name")
+//                shortcutIntent.data = Uri.parse("pinned://deeplink")
+//                this.sendBroadcast(shortcut)
+//            }
 
-                if (shortcutManager!!.isRequestPinShortcutSupported) {
-                    val pinShortcutInfo =
-                        ShortcutInfo.Builder(this, "my-shortcut")
-                            .setShortLabel("pinned shortcut")
-                            .setIntent(Intent(Intent.ACTION_VIEW, Uri.parse("pinned://deeplink")))
-                            .build()
+            val shortcutInfo: ShortcutInfoCompat =
+                ShortcutInfoCompat.Builder(this, "id")
+                    .setShortLabel("pinned shortcut")
+                    .setIntent(Intent(Intent.ACTION_VIEW, Uri.parse("pinned://deeplink")))
+                    .build()
 
-                    val pinnedShortcutCallbackIntent =
-                        shortcutManager.createShortcutResultIntent(pinShortcutInfo)
-
-                    val successCallback = PendingIntent.getBroadcast(
-                        this,
-                        0,
-                        pinnedShortcutCallbackIntent,
-                        0)
-
-                    shortcutManager.requestPinShortcut(pinShortcutInfo, successCallback.intentSender)
-                }
-            } else {
-            }
+            ShortcutManagerCompat.requestPinShortcut(this, shortcutInfo, null)
         }
-
     }
 }
